56a58cbcf0bd02bf7824af1d185447475bd54f7c
jottaxwds <hello@jottax.xyz>
2023-06-04 13:37:47 +0200

    refactor & README updates

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 const crypto = require("crypto");
 
+function createHashPartitionKey (data) {
+    return crypto.createHash("sha3-512").update(data).digest("hex")
+}
+
+function getHashPartitionKeyFromEvent (event) {
+    const data = JSON.stringify(event);
+    return createHashPartitionKey(data);
+}
+
+function getPartitionKeyFromEvent (event) {
+    if (!event) {
+        return null;
+    }
+
+    if (event.partitionKey) {
+        return event.partitionKey;
+    } 
+
+    return getHashPartitionKeyFromEvent(event);    
+}
+
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+  let candidate = getPartitionKeyFromEvent(event);
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (!candidate) {
+    return TRIVIAL_PARTITION_KEY;
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+    return createHashPartitionKey(candidate);
   }
+
+  if (typeof candidate !== "string") {
+    return JSON.stringify(candidate);
+  }
+
   return candidate;
 };
